0cde79c9b00b669452f3d38a589d5b14c4b8dac2
DavidSosaMoriana <sosamorianadav@gmail.com>
2022-09-27 17:44:48 +0100

    :zap: Date and reaction btn

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,25 +1,42 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { sub } from 'date-fns';
 
 const initialState = [
     {
         id: '1',
         title: 'Learning redux toolkit',
         content: "I've heard good things",
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+        },
     },
     {
         id: '2',
         title: 'Slices...',
         content: 'The more I say slice, the more I want pizza.',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+        },
     },
 ];
 
 const postsSlice = createSlice({
     name: 'posts',
-    initialState,   
+    initialState,
     reducers: {
         postAdded: {
             reducer(state, action) {
-                state.push(action.payload)
+                state.push(action.payload);
             },
             prepare(title, content, userId) {
                 return {
@@ -27,17 +44,32 @@ const postsSlice = createSlice({
                         id: nanoid(),
                         title,
                         content,
+                        date: new Date().toISOString(),
                         userId,
-                    }
-                }
+                        reactions: {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0,
+                        },
+                    },
+                };
+            },
+        },
+        reactionAdded(state, action) {
+            const { postId, reaction } = action.payload;
+            const existingPost = state.find((post) => post.id === postId);
+            if (existingPost) {
+                existingPost.reactions[reaction]++;
             }
-        }
+        },
     },
 });
 
 //Exportamos esta función para no tener que cambiar el estado de cada post en los diferentes componentes
 export const selectAllPosts = (state) => state.posts;
 
-export const { postAdded } = postsSlice.actions
+export const { postAdded, reactionAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
